38eec0c90466765da2f5f75574f0f10004dcd276
Thilan Senanayake <thilansenanayake@gmail.com>
2020-09-27 11:50:00 +0530

    Changed text in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo My name is Thilan
+          This change was made in the feature branch
         </p>
         <a
           className="App-link"
